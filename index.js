7d4054b6107a56d8bf2449028f64eec3ad3c510a
Pierre Rolland <pierre.rolland@openclassrooms.com>
2021-05-07 18:19:16 +0200

    Added files matrix

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const convert = require('xml-js');
 
 const ACTION = core.getInput('action');
 const COVERAGE_BRANCH = 'coverage';
-const COVERAGE_FILE = core.getInput('coverage-file');
-const SUMMARY_FILE = core.getInput('summary-file');
+const COVERAGE_FILES = JSON.parse(core.getInput('files'));
 const TOKEN = core.getInput('token');
 const REPO = `https://${process.env.GITHUB_ACTOR}:${TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
 
@@ -21,7 +20,7 @@ const fail = (message) => {
 };
 
 const execute = (command, options) => new Promise(function (resolve, reject) {
-    const cb = (error, stdout) => {
+    const cb = (error, stdout, stderr) => {
         if (error) {
             core.setFailed(error);
             reject(error);
@@ -81,8 +80,8 @@ const push = async (cwd) => {
     await execute(`git push ${REPO} HEAD`, {cwd});
 };
 
-const parseCoverage = async () => {
-    const globber = await glob.create(COVERAGE_FILE);
+const parseCoverage = async file => {
+    const globber = await glob.create(file);
     const files = await globber.glob();
 
     if (files.length === 0) {
@@ -101,15 +100,42 @@ const parseCoverage = async () => {
     return {total, covered, coverage};
 }
 
-const fetchBaseCoverage = () => fetch(`https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/${COVERAGE_BRANCH}/${SUMMARY_FILE}`, {
+const parseCoverages = async () => {
+    const reports = {};
+
+    for (const file of COVERAGE_FILES) {
+        reports[file.summary] = await parseCoverage(file.coverage);
+    }
+
+    return reports;
+};
+
+const fetchBaseCoverage = summaryFile => fetch(`https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/${COVERAGE_BRANCH}/${summaryFile}`, {
     headers: {
         'Authorization': `token ${TOKEN}`,
         'Accept': 'application/vnd.github.v3.raw'
     }
 });
 
+const sumCoverages = coverages => {
+    const out = {
+        total: 0,
+        covered: 0
+    };
+
+    for (const coverage of Object.values(coverages)) {
+        out.total += coverage.total;
+        out.covered += coverage.covered;
+    }
+
+    out.coverage = parseFloat((100 * out.covered / out.total).toFixed(3));
+
+    return out;
+}
+
 const postMessageOnPullRequest = async message => {
     const context = github.context;
+
     console.log(message);
 
     if (context.payload.pull_request == null) {
@@ -146,37 +172,61 @@ const buildSuccessMessage = (oldCoverage, newCoverage) => {
     return ':white_check_mark: Your code coverage has not been degraded :tada:' + buildDeltaMessage(oldCoverage, newCoverage);
 };
 
-const update = async coverage => {
+const buildResultMessage = (oldCoverage, newCoverage) => {
+    if (newCoverage.coverage < oldCoverage.coverage) {
+        core.setFailed('Code coverage has been degraded');
+
+        return buildFailureMessage(oldCoverage, newCoverage);
+    }
+
+    return buildSuccessMessage(oldCoverage, newCoverage);
+}
+
+const update = async coverages => {
     const workingDir = await clone();
-    fs.writeFileSync(`${workingDir}/${SUMMARY_FILE}`, JSON.stringify(coverage));
+
+    for (const summaryFile of Object.keys(coverages)) {
+        fs.writeFileSync(`${workingDir}/${summaryFile}`, JSON.stringify(coverages[summaryFile]));
+    }
+
     await push(workingDir);
 
     console.log('Coverage successfully updated');
 };
 
-const check = async coverage => {
-    const baseCoverageResult = await fetchBaseCoverage();
+const check = async coverages => {
+    const baseCoverages = {};
+    const messages = [];
 
-    if (baseCoverageResult.status === 404) {
-        console.log(`No base coverage found. Current coverage is ${coverage.coverage}% (${coverage.total} lines, ${coverage.covered} covered)`);
-        return;
+    for (const summaryFile of Object.keys(coverages)) {
+        const baseCoverageResult = await fetchBaseCoverage(summaryFile);
+        const coverage = coverages[summaryFile];
+
+        if (baseCoverageResult.status === 404) {
+            console.log(`No base coverage ${summaryFile} found. Current coverage is ${coverage.coverage}% (${coverage.total} lines, ${coverage.covered} covered)`);
+            continue;
+        }
+
+        baseCoverages[summaryFile] = await baseCoverageResult.json();
+
+        messages.push(summaryFile + '\n' + buildResultMessage(baseCoverages[summaryFile], coverage));
     }
 
-    const coverageResult = await baseCoverageResult.json();
+    if (Object.keys(coverages).length > 1) {
+        const globalBaseCoverage = sumCoverages(baseCoverages);
+        const globalCoverage = sumCoverages(coverages);
 
-    if (coverage.coverage < coverageResult.coverage) {
-        await postMessageOnPullRequest(buildFailureMessage(coverageResult, coverage));
-        core.setFailed('Code coverage has been degraded');
-    } else {
-        await postMessageOnPullRequest(buildSuccessMessage(coverageResult, coverage));
+        messages.push('global\n' + buildResultMessage(globalBaseCoverage, globalCoverage));
     }
+
+    await postMessageOnPullRequest(messages.join('\n---\n'));
 };
 
 const action = async () => {
     try {
-        const coverage = await parseCoverage();
+        const coverages = await parseCoverages();
 
-        await (ACTION === 'update' ? update(coverage) : check(coverage));
+        await (ACTION === 'update' ? update(coverages) : check(coverages));
     } catch (error) {
         core.setFailed(error.message);
     }
